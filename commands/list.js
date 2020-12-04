54bce4c7491ea5f9ca9b7a07165433ac26588491
Oscurito <waffufaccia@gmail.com>
2020-12-04 10:45:24 +0100

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -9,7 +9,7 @@ module.exports = {
 			color: 6166501,
 			footer: {
 			  icon_url: "https://yt3.ggpht.com/ytc/AAUvwniIz_ZP4P3fjwh7E9Ah2EGUQB7rWg1CxCwmdyX_5w=s900-c-k-c0x00ffffff-no-rj",
-			  text: "| Made by Oscurito"
+			  text: "For bugs dm 🙒 Oscurito 🙑#8849 | Made by Oscurito "
 			},
 			image: {
 				url: "https://scontent.ccdn.cloud/image/nospoiler/ab58e801-f415-4e1d-8c0a-14c33eb52c16.gif"
